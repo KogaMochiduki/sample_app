f24a5fb94a4b961a44971a1711d0b2db854bffb1
KogaMochiduki <b1646038@planet.kanazawa-it.ac.jp>
2022-02-17 16:20:34 +0000

    Add presence test of users

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -14,8 +14,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
     get contact_path
+    assert_template 'static_pages/contact'
     assert_select "title", full_title("Contact")
     get signup_path
+    assert_template 'users/new'
     assert_select "title", full_title("Sign up")
   end
 end
