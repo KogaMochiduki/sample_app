1566df5c410b4b90b3d38ea711659bf064558877
KogaMochiduki <b1646038@planet.kanazawa-it.ac.jp>
2022-03-28 15:58:51 +0000

    Create view for users index

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,6 +5,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
   
+  def setup
+    @user = users(:michael)
+  end
+  
    test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -19,5 +23,15 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_template 'users/new'
     assert_select "title", full_title("Sign up")
+  end
+  
+  test "layout links when logged in" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)    
+    assert_select "a[href=?]", logout_path
   end
 end
