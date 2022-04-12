5340ba7e62851c583d950df9a786082ae775daea
KogaMochiduki <b1646038@planet.kanazawa-it.ac.jp>
2022-04-12 12:58:28 +0000

    mailer preview setting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,9 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  host = '28275117c6484545ba245c0080f0b135.vfs.cloud9.us-east-2.amazonaws.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
