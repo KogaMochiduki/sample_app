6ef8d71b928e483a7d2df95997b85f42ebbff42e
KogaMochiduki <b1646038@planet.kanazawa-it.ac.jp>
2022-05-04 14:02:38 +0000

    Add test for micropost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,16 +53,8 @@ class UsersController < ApplicationController
                                  :password_confirmation)
   end
   
+  
   # beforeアクション
-
-  # ログイン済みユーザーかどうか確認
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
   
   def correct_user
     @user = User.find(params[:id])
